efcea0cb281f5bd29db1dfbb8f073d95d37cc87c
ElmiraAR <azifovna@gmail.com>
2021-12-11 20:03:42 +0500

    tabulatia

--- GBJavaLesson1.java
+++ GBJavaLesson1.java
@@ -1,39 +1,38 @@
 class GBJavaLesson1 {
     public static void main(String[] args) {
-	    printThreeWords();
-		checkSumSign();
-		printColor();
-		compareNumbers();
-	}
-	
-	static void printThreeWords() {
-	    System.out.println("Orange");
-	    System.out.println("Banana");
-	    System.out.println("Apple");
-	
-	}
-	static void checkSumSign() {
-	    int a=-5;
-		int b=5;
-		System.out.println((a+b>=0) ? "Sum is positive":"Sum is negative");
-	}
-	
-	static void printColor() {
-	    int value=0;
-		if (value<=0) {
-		    System.out.println("Red");
-		}
-		else if (value>0 && value<=100) {
-		    System.out.println("Yellow");
-		} 
-		else{
-		    System.out.println("Green");
-		}
-	}
-	
-	static void compareNumbers() {
-	    int a=1;
-		int b=20;
-		System.out.println((a>=b) ? "a>=b":"a<b");
-	}
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    static void checkSumSign() {
+        int a=-5;
+        int b=5;
+        System.out.println((a+b>=0) ? "Sum is positive":"Sum is negative");
+    }
+
+    static void printColor() {
+        int value=0;
+        if (value<=0) {
+        System.out.println("Red");
+        }
+        else if (value>0 && value<=100) {
+        System.out.println("Yellow");
+        } 
+        else{
+        System.out.println("Green");
+        }
+    }
+
+    static void compareNumbers() {
+        int a=1;
+        int b=20;
+        System.out.println((a>=b) ? "a>=b":"a<b");
+    }
 }
